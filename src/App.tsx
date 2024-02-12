06872a63635b728feadd460432ff37eac48b492b
Bressendorff <95355589+Bressendorff@users.noreply.github.com>
2024-02-12 09:55:08 +0100

    Added dynamic grid layout on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Grid
+        templateAreas={{
+          base: `"nav"
+                  "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem bg="orange.300" gridArea={"nav"}>
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem bg="pink.300" gridArea={"aside"}>
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem bg="green.300" gridArea={"main"}>
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
